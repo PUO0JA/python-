d4d9b23421977b4872a951ef2db8ed839517bfe7
PUO0JA <40468546+PUO0JA@users.noreply.github.com>
2020-04-11 20:42:30 +1300

    Add files via upload

---
+++ python8.py
@@ -0,0 +1,13 @@
+lis=['map' , 'len' , 'input' , 'range' , 'print']
+print('The length function work as follows:')
+print('The length of this list is', len(lis))
+print('The map function works as follows:')
+print('Here it is listing the list of alphabets of strings in an individual manner')
+fun=list(map(list, lis))
+print(fun)
+
+for i in range(len(lis)):
+    print('Hi this is element',lis[i])
+print('The range function decides the scope of the program')
+
+
