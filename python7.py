a906dc71d1a7ef3e9d40b948caf6e0c36c17312e
PUO0JA <40468546+PUO0JA@users.noreply.github.com>
2020-04-11 20:40:49 +1300

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,17 @@
+a=int(input("Enter a number 1"))
+b=int(input("Enter a number 2"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a//b
+i=a**b
+print(type(a),type(b))
+print(type(c),c)
+print(type(d),d)
+print(type(e),e)
+print(type(f),f)
+print(type(g),g)
+print(type(h),h)
+print(type(i),i)
