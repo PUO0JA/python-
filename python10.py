d3ce2c0a0f30642bfdf89858b00738c9b8440685
PUO0JA <40468546+PUO0JA@users.noreply.github.com>
2020-04-16 04:11:55 +1300

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,10 +1,10 @@
 def make_them_feel_bad(userlist):
     exp=''.join(userlist)
-    print('you are such a ' + exp)
+    print(Name + ' is such a ' + exp)
     return(exp)
  
 
-
+Name=str(input(print('Enter the name of the person you want to make feel good(sarcastically)')))
 input_expressions=str(input('Enter the list of expressions you want to use separated by spaces'))
 userlist=input_expressions.split()
 print('Your list of expressions is', userlist)
